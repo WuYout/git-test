4c0af5a4eac611014ffae6e43f702ba9d419d94f
Magelite <maragretoy@163.com>
2022-11-12 15:58:17 +0800

    first commit

---
+++ src/main/java/com/margaret/springcloud/GitTest.java
@@ -0,0 +1,13 @@
+package com.margaret.springcloud;
+
+/**
+ * @author margaret
+ * @version 1.0
+ * @date 2022/11/12 15:54
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+
+}
