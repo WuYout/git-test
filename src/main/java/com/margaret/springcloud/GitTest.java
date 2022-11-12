9706f57569f8853824e05edbd65fcff85553ec28
Magelite <maragretoy@163.com>
2022-11-12 16:46:13 +0800

    master commit2

--- src/main/java/com/margaret/springcloud/GitTest.java
+++ src/main/java/com/margaret/springcloud/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("Hello Git2");
         System.out.println("Hello Git3");
         System.out.println("Hello Git4");
+        System.out.println("master test!");
 
     }
 
