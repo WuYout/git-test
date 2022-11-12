13d5765bb50e92382fe20a18218e9a1acf34076e
Magelite <maragretoy@163.com>
2022-11-12 16:00:31 +0800

    sec commit

--- src/main/java/com/margaret/springcloud/GitTest.java
+++ src/main/java/com/margaret/springcloud/GitTest.java
@@ -8,6 +8,8 @@ package com.margaret.springcloud;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git2");
+
     }
 
 }
