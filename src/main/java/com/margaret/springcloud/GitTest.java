1b90e482dd042137a6407de2b1a55499cf95ffd5
Magelite <maragretoy@163.com>
2022-11-12 16:43:38 +0800

    hot-rix commit2

--- src/main/java/com/margaret/springcloud/GitTest.java
+++ src/main/java/com/margaret/springcloud/GitTest.java
@@ -9,8 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Hello Git2");
-        System.out.println("Hello Git3");
-        System.out.println("Hello Git4");
 
     }
 
