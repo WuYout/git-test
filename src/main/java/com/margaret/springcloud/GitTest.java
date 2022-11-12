bd81d315b7df25172ae9e5db6b6c0bd284f525e0
WuYout <59219979+WuYout@users.noreply.github.com>
2022-11-12 18:02:41 +0800

    pull commit

--- src/main/java/com/margaret/springcloud/GitTest.java
+++ src/main/java/com/margaret/springcloud/GitTest.java
@@ -14,6 +14,7 @@ public class GitTest {
         System.out.println("master test!");
         System.out.println("hot-fix test!");
         System.out.println("push test!");
+        System.out.println("pull test!");
 
     }
 
